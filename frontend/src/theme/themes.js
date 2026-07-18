630a824762ddf02029f115067aa1d7218f311cdc
Lemonsszz <axelrcm777@gmail.com>
2026-07-18 15:21:42 -0400

    Otro commit

---
+++ frontend/src/theme/themes.js
@@ -0,0 +1,25 @@
+export const themes = {
+    cyberPurple: {
+        background: "#0B1120",
+        surface: "#111827",
+        card: "#1E293B",
+        primary: "#7C3AED",
+        text: "#F8FAFC"
+    },
+
+    hacker: {
+        background: "#000000",
+        surface: "#050505",
+        card: "#001A0A",
+        primary: "#00FF66",
+        text: "#E6FFE6"
+    },
+
+    light: {
+        background: "#FFFFFF",
+        surface: "#F3F4F6",
+        card: "#FFFFFF",
+        primary: "#4F46E5",
+        text: "#111827"
+    }
+};
